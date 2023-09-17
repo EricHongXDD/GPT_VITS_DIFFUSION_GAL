038db905a36c915687ce6c863f4ec2d9832fa74b
Hong <1029277025@qq.com>
2023-09-17 14:53:50 +0800

    Add files via upload

---
+++ vits_vits.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import os
+
+def vits(message,people,language,speed,key,sheet,character):
+    url = "http://127.0.0.1:7860"
+
+    payload = {
+        "data": [message, people, language, int(speed)],
+        "event_data": None,
+        "fn_index": 0,
+
+    }
+
+    session = requests.Session()
+    session.cookies.clear()
+    result = requests.post(url=f'{url}/run/predict', json=payload)
+
+    data = json.loads(result.text)
+    res = str(data["data"][1]["name"])
+    res = "http://127.0.0.1:7860/file=" + res
+    # print(res)
+
+    # 确保响应状态是200，这意味着请求是成功的
+    response = requests.get(res, stream=True)
+    if response.status_code == 200:
+        # 确保 'audio' 文件夹存在
+        if not os.path.exists('audio'):
+            os.makedirs('audio')
+
+        # 将音频文件保存到 'audio' 文件夹
+        filepath = "audio/"+str(sheet)+"_"+str(character)+"_"+str(key)+".wav"
+        with open(filepath, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+        return True
+    else:
+        print(f"Error: Unable to download file. Status code: {response.status_code}")
+        return False
